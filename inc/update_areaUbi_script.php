29cab55e375bf1614121ff3207a5365e8dc689ca
Giuseppe Naponiello <beppenapo@arc-team.com>
2016-02-18 12:29:27 +0100

    1602181220 fixed update ubicazioni

--- inc/update_areaUbi_script.php
+++ inc/update_areaUbi_script.php
@@ -5,19 +5,11 @@ $old_ubi= $_POST['old_ubi'];
 $area_update= $_POST['area_update'];
 $motiv_update= $_POST['motiv_update'];
 $noteUbiUpdate=pg_escape_string($_POST['noteUbiUpdate']);
-if ($motiv_update && $area_update){
-  $update = ("
-    BEGIN;
-    UPDATE aree_scheda SET id_area = $area_update, id_motivazione = $motiv_update  WHERE id_scheda = $id AND id = $old_ubi;
-    UPDATE scheda SET noteubi = '$noteUbiUpdate' WHERE id = $id;
-    COMMIT;
-  ");
-  $result = pg_query($connection, $update);
-  if(!$result){die("Errore nella query: \n" . pg_last_error($connection));}
-  else {die("Salvataggio avvenuto correttamente" . pg_last_error($connection));}
-}
-else{
-    die("Devi selezionare il campo ".($motiv_update?'Località' : 'Motivazione'));
-}
-
+$update ="BEGIN;
+            UPDATE aree_scheda SET id_area = $area_update, id_motivazione = $motiv_update  WHERE id_scheda = $id AND id = $old_ubi;
+            UPDATE scheda SET noteubi = '$noteUbiUpdate' WHERE id = $id;
+          COMMIT;";
+$result = pg_query($connection, $update);
+if(!$result){die("Errore nella query: \n" . pg_last_error($connection));}
+else {die("Salvataggio avvenuto correttamente" . pg_last_error($connection));}
 ?>
