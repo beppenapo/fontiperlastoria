32704866f4fab23434c137b9fe7e8ac4225db258
Giuseppe Naponiello <beppenapo@arc-team.com>
2016-02-19 12:49:52 +0100

    1602191250 fixed bug popup ubi da webgis

--- inc/popupUbi.php
+++ inc/popupUbi.php
@@ -3,7 +3,7 @@ include('db.php');
 
 $id = $_POST['id_ubi'];
 $tipo = $_POST['tipo'];
-$q=("SELECT localita.localita FROM ubicazione, aree, localita WHERE ubicazione.area = aree.nome_area AND aree.id_localita = localita.id AND ubicazione.id = $id");
+$q=("SELECT localita.localita FROM ubicazione, aree, localita WHERE ubicazione.id_area = aree.nome_area AND aree.id_localita = localita.id AND ubicazione.id = $id");
 $r=pg_query($connection, $q);
 $row =pg_num_rows($r);
 $arr = pg_fetch_array($r);
@@ -12,27 +12,27 @@ echo "<div class='titLocUbi'>".stripslashes($arr['localita'])."</div>";
 $query = ("
 with listone as(
  with a as
-  (SELECT madre.id AS id_madre, madre.dgn_numsch AS madre, madre.livello AS liv_madre, figlia.id AS id_figlia, figlia.dgn_numsch AS figlia, figlia.livello AS liv_figlia FROM altrif_parenti alt_madre, ubicazione, aree, aree_scheda, scheda, scheda madre, scheda figlia WHERE alt_madre.id_scheda = scheda.id AND  alt_madre.id_scheda = madre.id AND alt_madre.id_parente = figlia.id AND ubicazione.area = aree.nome_area AND aree_scheda.id_area = aree.nome_area AND aree_scheda.id_scheda = scheda.id AND madre.livello = 1 AND ubicazione.id = $id)
-,b as 
-  (SELECT madre.id AS id_madre, madre.dgn_numsch AS madre, madre.livello AS liv_madre, figlia.id AS id_figlia, figlia.dgn_numsch AS figlia, figlia.livello AS liv_figlia FROM altrif_parenti alt_madre, ubicazione, aree, aree_scheda, scheda, scheda madre, scheda figlia WHERE alt_madre.id_scheda = scheda.id AND  alt_madre.id_scheda = madre.id AND alt_madre.id_parente = figlia.id AND ubicazione.area = aree.nome_area AND aree_scheda.id_area = aree.nome_area AND aree_scheda.id_scheda = scheda.id AND madre.livello = 2 AND ubicazione.id = $id)
-,c as 
-  (SELECT madre.id AS id_madre, madre.dgn_numsch AS madre, madre.livello AS liv_madre, figlia.id AS id_figlia, figlia.dgn_numsch AS figlia, figlia.livello AS liv_figlia FROM altrif_parenti alt_madre, ubicazione, aree, aree_scheda, scheda, scheda madre, scheda figlia WHERE alt_madre.id_scheda = scheda.id AND  alt_madre.id_scheda = madre.id AND alt_madre.id_parente = figlia.id AND ubicazione.area = aree.nome_area AND aree_scheda.id_area = aree.nome_area AND aree_scheda.id_scheda = scheda.id AND madre.livello = 3 AND ubicazione.id = $id)
-,d as 
-  (SELECT scheda.id as id_madre, scheda.dgn_numsch as madre FROM ubicazione, aree, aree_scheda, scheda WHERE ubicazione.area = aree.nome_area AND aree_scheda.id_area = aree.nome_area AND aree_scheda.id_scheda = scheda.id AND ubicazione.id = $id)
+  (SELECT madre.id AS id_madre, madre.dgn_numsch AS madre, madre.livello AS liv_madre, figlia.id AS id_figlia, figlia.dgn_numsch AS figlia, figlia.livello AS liv_figlia FROM altrif_parenti alt_madre, ubicazione, aree, aree_scheda, scheda, scheda madre, scheda figlia WHERE alt_madre.id_scheda = scheda.id AND  alt_madre.id_scheda = madre.id AND alt_madre.id_parente = figlia.id AND ubicazione.id_area = aree.nome_area AND aree_scheda.id_area = aree.nome_area AND aree_scheda.id_scheda = scheda.id AND madre.livello = 1 AND ubicazione.id = $id)
+,b as
+  (SELECT madre.id AS id_madre, madre.dgn_numsch AS madre, madre.livello AS liv_madre, figlia.id AS id_figlia, figlia.dgn_numsch AS figlia, figlia.livello AS liv_figlia FROM altrif_parenti alt_madre, ubicazione, aree, aree_scheda, scheda, scheda madre, scheda figlia WHERE alt_madre.id_scheda = scheda.id AND  alt_madre.id_scheda = madre.id AND alt_madre.id_parente = figlia.id AND ubicazione.id_area = aree.nome_area AND aree_scheda.id_area = aree.nome_area AND aree_scheda.id_scheda = scheda.id AND madre.livello = 2 AND ubicazione.id = $id)
+,c as
+  (SELECT madre.id AS id_madre, madre.dgn_numsch AS madre, madre.livello AS liv_madre, figlia.id AS id_figlia, figlia.dgn_numsch AS figlia, figlia.livello AS liv_figlia FROM altrif_parenti alt_madre, ubicazione, aree, aree_scheda, scheda, scheda madre, scheda figlia WHERE alt_madre.id_scheda = scheda.id AND  alt_madre.id_scheda = madre.id AND alt_madre.id_parente = figlia.id AND ubicazione.id_area = aree.nome_area AND aree_scheda.id_area = aree.nome_area AND aree_scheda.id_scheda = scheda.id AND madre.livello = 3 AND ubicazione.id = $id)
+,d as
+  (SELECT scheda.id as id_madre, scheda.dgn_numsch as madre FROM ubicazione, aree, aree_scheda, scheda WHERE ubicazione.id_area = aree.nome_area AND aree_scheda.id_area = aree.nome_area AND aree_scheda.id_scheda = scheda.id AND ubicazione.id = $id)
 SELECT a.id_madre, a.madre FROM a where exists(select * from a)
 UNION
-SELECT b.id_figlia, b.figlia FROM b where b.id_figlia in(select d.id_madre from d) AND b.id_madre not in(select a.id_figlia from a) or not exists(select * from a) AND b.liv_figlia = 2 AND NOT b.liv_madre = 2 
+SELECT b.id_figlia, b.figlia FROM b where b.id_figlia in(select d.id_madre from d) AND b.id_madre not in(select a.id_figlia from a) or not exists(select * from a) AND b.liv_figlia = 2 AND NOT b.liv_madre = 2
 UNION
-SELECT b.id_madre, b.madre FROM b where b.id_figlia not in(select d.id_madre from d) AND b.id_madre in(select d.id_madre from d)  AND b.liv_figlia = 1 AND b.liv_madre = 2 
+SELECT b.id_madre, b.madre FROM b where b.id_figlia not in(select d.id_madre from d) AND b.id_madre in(select d.id_madre from d)  AND b.liv_figlia = 1 AND b.liv_madre = 2
 UNION
 SELECT c.id_madre, c.madre FROM c where c.id_madre in(select d.id_madre from d) AND c.id_figlia not in(select a.id_madre from a) AND c.id_figlia not in(select b.id_madre from b) AND c.liv_madre = 3 AND NOT c.liv_figlia = 3
 UNION
 SELECT d.id_madre, d.madre FROM d where d.id_madre not in(select b.id_figlia from b) AND d.id_madre not in(select b.id_madre from b) AND d.id_madre not in(select c.id_madre from c)
 )
-SELECT listone.id_madre, listone.madre, css.css, cronologia.cro_spec as crono, scheda.dgn_dnogg as ogg, scheda.dgn_tpsch 
-FROM listone, lista_dgn_tpsch as css, cronologia, scheda 
+SELECT listone.id_madre, listone.madre, css.css, cronologia.cro_spec as crono, scheda.dgn_dnogg as ogg, scheda.dgn_tpsch
+FROM listone, lista_dgn_tpsch as css, cronologia, scheda
 where listone.id_madre = scheda.id AND cronologia.id_scheda = listone.id_madre AND scheda.dgn_tpsch = css.id AND scheda.dgn_tpsch = $tipo
-order by listone.madre ASC");
+order by listone.madre ASC;");
 $result = pg_query($connection, $query);
 $righe = pg_num_rows($result);
 echo "<div class='ulContent'><ul class='ulpopup'>";
